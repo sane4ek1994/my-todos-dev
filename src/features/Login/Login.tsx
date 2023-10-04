eaa4049b9e59a8f049843a9b04b7b4c149fd8fb5
Alexandr Naum <mfq07k9cz@gmail.com>
2023-10-04 17:51:40 +0300

    update auth reducer thunks, logout, login, initializedApp

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,77 +1,94 @@
-import React from 'react'
-import Grid from '@mui/material/Grid';
-import Checkbox from '@mui/material/Checkbox';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import FormLabel from '@mui/material/FormLabel';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import {useFormik} from "formik";
-import * as Yup from 'yup';
-import {loginTC} from "./auth-reducer";
-import {useAppDispatch, useAppSelector} from "app/store";
-import {Navigate} from "react-router-dom";
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Checkbox from "@mui/material/Checkbox";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import FormLabel from "@mui/material/FormLabel";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useAppDispatch, useAppSelector } from "app/store";
+import { Navigate } from "react-router-dom";
+import { authThunks } from "features/Login/auth-reducer";
 
 export const Login = () => {
-    const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
-    const dispatch = useAppDispatch()
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            rememberMe: false
-        },
-        validationSchema: Yup.object({
-            email: Yup.string().email('Invalid email address').required('Required'),
-            password: Yup.string().min(3, 'must be at least 3 characters long').required('Required')
-        }),
-        onSubmit: values => {
-            dispatch(loginTC(values))
-        },
-    })
+  const isLoggedIn = useAppSelector<boolean>((state) => state.auth.isLoggedIn);
+  const dispatch = useAppDispatch();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(3, "must be at least 3 characters long")
+        .required("Required"),
+    }),
+    onSubmit: (data) => {
+      dispatch(authThunks.login({ data }));
+    },
+  });
 
-    if (isLoggedIn) {
-        return <Navigate to={'/'}/>
-    }
+  if (isLoggedIn) {
+    return <Navigate to={"/"} />;
+  }
 
-
-    return <Grid container justifyContent={'center'}>
-        <Grid item justifyContent={'center'}>
-            <form onSubmit={formik.handleSubmit}>
-                <FormControl>
-                    <FormLabel>
-                        <p>To log in get registered
-                            <a href={'https://social-network.samuraijs.com/'}
-                               target={'_blank'} rel="noreferrer"> here
-                            </a>
-                        </p>
-                        <p>or use common test account credentials:</p>
-                        <p>Email: free@samuraijs.com</p>
-                        <p>Password: free</p>
-                    </FormLabel>
-                    <FormGroup>
-                        <TextField
-                            variant="standard"
-                            label="Email"
-                            margin="normal"
-                            {...formik.getFieldProps('email')}/>
-                        {formik.touched.email && <div style={{color: 'red'}}>{formik.errors.email}</div>}
-                        <TextField
-                            variant="standard"
-                            type="password"
-                            label="Password"
-                            margin="normal"
-                            {...formik.getFieldProps('password')}/>
-                        {formik.touched.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
-                        <FormControlLabel label={'Remember me'}
-                                          control={<Checkbox {...formik.getFieldProps('rememberMe')}/>}/>
-                        <Button type={'submit'} variant={'contained'} color={'primary'}>
-                            Login
-                        </Button>
-                    </FormGroup>
-                </FormControl>
-            </form>
-        </Grid>
+  return (
+    <Grid container justifyContent={"center"}>
+      <Grid item justifyContent={"center"}>
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl>
+            <FormLabel>
+              <p>
+                To log in get registered
+                <a
+                  href={"https://social-network.samuraijs.com/"}
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  {" "}
+                  here
+                </a>
+              </p>
+              <p>or use common test account credentials:</p>
+              <p>Email: free@samuraijs.com</p>
+              <p>Password: free</p>
+            </FormLabel>
+            <FormGroup>
+              <TextField
+                variant="standard"
+                label="Email"
+                margin="normal"
+                {...formik.getFieldProps("email")}
+              />
+              {formik.touched.email && (
+                <div style={{ color: "red" }}>{formik.errors.email}</div>
+              )}
+              <TextField
+                variant="standard"
+                type="password"
+                label="Password"
+                margin="normal"
+                {...formik.getFieldProps("password")}
+              />
+              {formik.touched.password && (
+                <div style={{ color: "red" }}>{formik.errors.password}</div>
+              )}
+              <FormControlLabel
+                label={"Remember me"}
+                control={<Checkbox {...formik.getFieldProps("rememberMe")} />}
+              />
+              <Button type={"submit"} variant={"contained"} color={"primary"}>
+                Login
+              </Button>
+            </FormGroup>
+          </FormControl>
+        </form>
+      </Grid>
     </Grid>
-}
+  );
+};
--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,73 +1,102 @@
-import { handleServerNetworkError} from "common/utils/handleServerNetworkError";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {AppThunk} from "app/store";
-import {appActions} from "app/app-reducer";
-import {handleServerAppError} from "common/utils/handleServerAppError";
+import { handleServerNetworkError } from "common/utils/handleServerNetworkError";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { appActions } from "app/app-reducer";
+import { handleServerAppError } from "common/utils/handleServerAppError";
 import { authAPI } from "./auth-api";
-import {RESULT_CODE} from "common/enums/enums";
-import {LoginDataType} from "common/types/types";
-
+import { RESULT_CODE } from "common/enums/enums";
+import { LoginDataType } from "common/types/types";
+import { createAppAsyncThunk } from "common/utils/createAppAsyncThunk";
 
 const slice = createSlice({
-    name: 'auth',
-    initialState: {
-        isLoggedIn: false
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+  },
+  reducers: {
+    setIsLoggedIn: (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
+      state.isLoggedIn = action.payload.isLoggedIn;
     },
-    reducers: {
-        setIsLoggedIn: (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
-            state.isLoggedIn = action.payload.isLoggedIn
-        }
-    }
-})
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(authThunks.login.fulfilled, (state, action) => {
+        state.isLoggedIn = action.payload.isLoggedIn;
+      })
+      .addCase(authThunks.logout.fulfilled, (state, action) => {
+        state.isLoggedIn = action.payload.isLoggedIn;
+      })
+      .addCase(authThunks.initializeApp.fulfilled, (state, action) => {
+        state.isLoggedIn = action.payload.isLoggedIn;
+      });
+  },
+});
 
-export const authReducer = slice.reducer
-// actions
-export const authActions = slice.actions
 // thunks
-export const loginTC = (data: LoginDataType): AppThunk => async (dispatch) => {
-    dispatch(appActions.setAppStatus({status: 'loading'}))
-    try {
-        const result = await authAPI.login(data)
-        if (result.data.resultCode === RESULT_CODE.SUCCESS) {
-            dispatch(authActions.setIsLoggedIn({isLoggedIn: true}))
-            dispatch(appActions.setAppStatus({status: 'succeeded'}))
-        } else {
-            handleServerAppError(result.data, dispatch)
-        }
-    } catch (e) {
-        handleServerNetworkError(e as string, dispatch)
-    }
-}
 
-export const logoutTC = (): AppThunk => async (dispatch) => {
-    dispatch(appActions.setAppStatus({status: 'loading'}))
-    try {
-        const result = await authAPI.logout()
-        if (result.data.resultCode === RESULT_CODE.SUCCESS) {
-            dispatch(authActions.setIsLoggedIn({isLoggedIn: false}))
-            dispatch(appActions.setAppStatus({status: 'succeeded'}))
-        } else {
-            handleServerAppError(result.data, dispatch)
-        }
-    } catch (e) {
-        handleServerNetworkError(e as string, dispatch)
+const login = createAppAsyncThunk<
+  { isLoggedIn: boolean },
+  { data: LoginDataType }
+>(`${slice.name}/login`, async ({ data }, thunkAPI) => {
+  const { dispatch, rejectWithValue } = thunkAPI;
+  dispatch(appActions.setAppStatus({ status: "loading" }));
+  try {
+    const result = await authAPI.login(data);
+    if (result.data.resultCode === RESULT_CODE.SUCCESS) {
+      dispatch(appActions.setAppStatus({ status: "succeeded" }));
+      return { isLoggedIn: true };
+    } else {
+      handleServerAppError(result.data, dispatch);
+      return rejectWithValue(null);
     }
-}
+  } catch (e) {
+    handleServerNetworkError(e as string, dispatch);
+    return rejectWithValue(null);
+  }
+});
 
-export const meTC = (): AppThunk => async (dispatch) => {
-    dispatch(appActions.setAppStatus({status: 'loading'}))
+const logout = createAppAsyncThunk<{ isLoggedIn: boolean }, undefined>(
+  `${slice.name}/logout`,
+  async (_, thunkAPI) => {
+    const { dispatch, rejectWithValue } = thunkAPI;
+    dispatch(appActions.setAppStatus({ status: "loading" }));
     try {
-        const result = await authAPI.me()
-        if (result.data.resultCode === RESULT_CODE.SUCCESS) {
-            dispatch(authActions.setIsLoggedIn({isLoggedIn: true}))
-            dispatch(appActions.setAppStatus({status: 'succeeded'}))
-        } else {
-            handleServerAppError(result.data, dispatch)
-        }
+      const result = await authAPI.logout();
+      if (result.data.resultCode === RESULT_CODE.SUCCESS) {
+        dispatch(appActions.setAppStatus({ status: "succeeded" }));
+        return { isLoggedIn: false };
+      } else {
+        handleServerAppError(result.data, dispatch);
+        return rejectWithValue(null);
+      }
     } catch (e) {
-        handleServerNetworkError(e as string, dispatch)
+      handleServerNetworkError(e as string, dispatch);
+      return rejectWithValue(null);
+    }
+  }
+);
+
+const initializeApp = createAppAsyncThunk<{ isLoggedIn: boolean }, undefined>(
+  `${slice.name}/initializeApp`,
+  async (_, thunkAPI) => {
+    const { dispatch, rejectWithValue } = thunkAPI;
+    try {
+      const result = await authAPI.me();
+      if (result.data.resultCode === RESULT_CODE.SUCCESS) {
+        dispatch(appActions.setAppStatus({ status: "succeeded" }));
+        return { isLoggedIn: true };
+      }
+      dispatch(appActions.setAppStatus({ status: "succeeded" }));
+      return rejectWithValue(null);
+    } catch (e) {
+      handleServerNetworkError(e as string, dispatch);
+      return rejectWithValue(null);
     } finally {
-        dispatch(appActions.setIsInitialized({isInitialized: true}))
+      dispatch(appActions.setIsInitialized({ isInitialized: true }));
     }
-}
+  }
+);
 
+export const authReducer = slice.reducer;
+export const authActions = slice.actions;
+
+export const authThunks = { login, logout, initializeApp };
