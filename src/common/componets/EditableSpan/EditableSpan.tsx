4815b1e2be54c33d746b5e4a9868eece5e687fab
Alexandr Naum <mfq07k9cz@gmail.com>
2023-10-22 14:10:41 +0300

    hot fix

--- src/common/componets/EditableSpan/EditableSpan.tsx
+++ src/common/componets/EditableSpan/EditableSpan.tsx
@@ -1,28 +1,39 @@
-import React, {ChangeEvent, useState} from 'react';
-import {TextField} from "@mui/material";
+import React, { ChangeEvent, useState } from "react";
+import { TextField } from "@mui/material";
+import { truncateString } from "common/utils/truncate-string";
 
 type TEditableSpan = {
-    title: string
-    onChangeTitle: (newTitle: string) => void
-}
-export const EditableSpan = React.memo(({title, onChangeTitle}: TEditableSpan) => {
+  title: string;
+  onChangeTitle: (newTitle: string) => void;
+};
+export const EditableSpan = React.memo(
+  ({ title, onChangeTitle }: TEditableSpan) => {
     const [editMode, setEditMode] = useState(false);
     const [newTitle, setNewTitle] = useState(title);
 
+    const shortTitle = truncateString(title, 10);
     const activateEditMode = () => {
-        setEditMode(true)
-    }
+      setEditMode(true);
+    };
 
     const activateViewMode = () => {
-        setEditMode(false)
-        onChangeTitle(newTitle)
-    }
-    const changeNewTitle = (e: ChangeEvent<HTMLInputElement>) => setNewTitle(e.currentTarget.value)
+      setEditMode(false);
+      onChangeTitle(newTitle);
+    };
+    const changeNewTitle = (e: ChangeEvent<HTMLInputElement>) =>
+      setNewTitle(e.currentTarget.value);
 
-    return (
-        editMode ?
-            <TextField variant="standard" onBlur={activateViewMode} autoFocus onChange={changeNewTitle} value={newTitle}
-                       type="text"/>
-            : <span onDoubleClick={activateEditMode}>{title}</span>
+    return editMode ? (
+      <TextField
+        variant="standard"
+        onBlur={activateViewMode}
+        autoFocus
+        onChange={changeNewTitle}
+        value={newTitle}
+        type="text"
+      />
+    ) : (
+      <span onDoubleClick={activateEditMode}>{shortTitle}</span>
     );
-});
+  }
+);
